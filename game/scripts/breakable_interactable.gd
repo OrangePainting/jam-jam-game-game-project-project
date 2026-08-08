a86be8c0e66db4af4548742f110180d7316ac2dc
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2026-08-07 20:07:15 -0700

    Make breaking acorn on rock
    breakable_interactable uses the name of the object it will break on to check

---
+++ game/scripts/breakable_interactable.gd
@@ -0,0 +1,30 @@
+class_name BreakableInteractable
+extends Interactable
+
+## Can be picked up and breaks on contact with objects named break_on_name
+## 
+## Can be picked up and dropped inherited from Interactable 
+## Breaks on contact with objects named break_on_name
+
+@export var break_on_name: String = ""
+
+var broken: bool = false;
+
+func _ready() -> void:
+	if break_on_name == "":
+		push_error("%s's break_on_name is empty" % name)
+	
+	super()
+
+func _on_body_entered(body: Node) -> void: 
+	# If the body is named break_on_name
+	if broken == false and body.name.match(break_on_name):
+		_break_open()
+
+func _on_zone_entered(area: Area2D) -> void: # No functionality
+	pass
+
+# Controls the breaking open animation
+func _break_open():
+	print("%s broke!" % name)
+	broken = true
