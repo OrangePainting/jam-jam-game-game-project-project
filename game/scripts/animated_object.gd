a7a32ae852c8c53c0eb936c52739a78362a5d905
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2026-08-08 19:45:44 -0700

    Add trigger interactable and animated object for prototyping
    animated_object can be called through _on_trigger which allows it to receive the interacted_with signal

---
+++ game/scripts/animated_object.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+## Generic class to cause animations to play when signals are triggered.
+## Connect via the signal editor to _on_trigger
+
+func play_animation() -> void:
+	$AnimationPlayer.play("test_anim")
+
+## External function for receiving signals
+func _on_trigger(_dummy: Node2D) -> void:
+	play_animation()
