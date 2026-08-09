36d2b7cc0a9da08b221d0898cc322fd2ac3930ca
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2026-08-09 03:39:01 -0700

    Add coconut and hammer

---
+++ game/scripts/hammer.gd
@@ -0,0 +1,10 @@
+extends Interactable
+
+## Doesn't have any more functionality than Interactable
+## Exists to override the _on_body_entered and _on_zone_entered
+
+func _on_body_entered(_body: Node) -> void: # No functionality
+	pass
+
+func _on_zone_entered(_area: Area2D) -> void: # No functionality
+	pass
