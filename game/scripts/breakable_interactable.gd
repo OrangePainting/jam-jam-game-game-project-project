61cbbb8b8428248fac2f342e1422368d232a71e7
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2026-08-09 03:58:16 -0700

    Add velocity gate for breaking objects
    When a breakable object comes in contact with it's "tool" it checks whether the object itself or the tool is moving fast enough

--- game/scripts/breakable_interactable.gd
+++ game/scripts/breakable_interactable.gd
@@ -7,6 +7,7 @@ extends Interactable
 ## Breaks on contact with objects named break_on_name
 
 @export var break_on_name: String = ""
+@export var velocity_needed: float = 10
 
 var broken: bool = false;
 
@@ -14,10 +15,25 @@ func _on_body_entered(body: Node) -> void:
 	# If the body is named break_on_name
 	if (not broken and not is_held and not break_on_name.is_empty() 
 	and body.name.match(break_on_name)):
-		_break_open()
+		_try_break_open(body)
 
 func _on_zone_entered(_area: Area2D) -> void: # No functionality
 	pass
+
+func _try_break_open(body: Node) -> bool:
+	# Checks if either this object or the object it's interacting with
+	# are moving fast enough
+	print("Linear velocity was %s" % linear_velocity.length())
+	if linear_velocity.length() >= velocity_needed:
+		_break_open()
+		return true
+	elif body is RigidBody2D and body.linear_velocity.length() >= velocity_needed:
+		print("Linear velocity was %s" % body.linear_velocity.length())
+		_break_open()
+		return true
+	else:
+		pass # Something happens to show it wasn't fast enough
+	return false
 
 # Controls the breaking open animation
 func _break_open():
