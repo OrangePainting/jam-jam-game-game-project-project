5c9d4daf829fc0771ba53762d8ba2be0b7f40e37
OrangePainting <krishkon4065@gmail.com>
2026-08-09 21:08:37 -0500

    Make basic clipboard logic

--- game/scripts/clipboard_sprite.gd
+++ game/scripts/clipboard_sprite.gd
@@ -1,12 +1,30 @@
 extends Control
 
 @onready var sprite = %ClipboardSprite
+@onready var bullet_text = %Bullets
+var things_completed: int = 0
+
+
+var clipboard_bullets: Array[String] = [
+	"Acorn",
+	"Peanut",
+	"Coconut",
+	"Lockbox",
+	"Vault",
+	"Acorn Statue"
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	bullet_text.text = ""
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var t = ""
+	for i in range(len(clipboard_bullets)):
+		if i < things_completed:
+			t += "[s color = 'green']" + clipboard_bullets[i] + "[/s]" + "\n"
+		else:
+			t += clipboard_bullets[i] + "\n"
+	bullet_text.text = "[ul]" + t + "[/ul]"
