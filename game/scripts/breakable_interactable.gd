c6791bbb0f0276e38de98e3d68f81f4260d35c7c
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2026-08-09 18:40:25 -0700

    Make breakable interactable able to drop other items

--- game/scripts/breakable_interactable.gd
+++ game/scripts/breakable_interactable.gd
@@ -12,6 +12,8 @@ extends Interactable
 @export var crack_index: int = 0
 @export var no_crack_sound_fail: String = ""
 
+@export var drop_on_break: PackedScene = null
+
 var broken: bool = false;
 
 func _on_body_entered(body: Node) -> void:
@@ -40,11 +42,11 @@ func _try_break_open(body: Node) -> bool:
 		if AudioController.has_method(no_crack_sound_fail):
 			AudioController.call(no_crack_sound_fail)
 	
-	
 	return false
 
 # Controls the breaking open animation
 func _break_open():
+	if broken: return
 	print("%s broke!" % name)
 	broken = true
 	set_deferred("freeze", true)
@@ -52,6 +54,10 @@ func _break_open():
 	$AnimatedSprite2D.play("Cracked")
 	if AudioController.has_method(crack_sound):
 		AudioController.call(crack_sound, crack_index)
+	if drop_on_break != null:
+		var object = drop_on_break.instantiate()
+		object.position = position
+		get_node("..").add_child(object) # Puts node on parent of this node
 
 func _on_place_interactable_item_placed(interactor: Node2D, item: Interactable) -> void:
 	if not broken:
